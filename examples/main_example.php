b9eaf4ed0c104ffde659f7c0d2455eb583ad155f
Jonathan Reichelt Gjertsen <jonathrg@stud.ntnu.no>
2017-06-18 01:09:07 +0200

    Rename check to call and use a better main example.

--- examples/main_example.php
+++ examples/main_example.php
@@ -4,26 +4,29 @@ require __DIR__.'/../src/Proposition.php';
 
 use Proposition\Proposition;
 
-// Test whether our assumption, that $a is always bigger than $b, holds for all integers passed into it.
+// Here's a toy example showing how you would use Proposition to check whether your awesome reimplementation of
+// abs($a) == abs($b) actually works.
+
+function equal_magnitude($a, $b)
+{
+    return ($a = $b) || ($a == -$b);
+}
 
 $proposition = new Proposition();
 
 $proposition
-    // First, we define the values to pass in with given()...
-    ->given(
-        Proposition::integerRange(5, 20),  // Values from here will be passed as $a
-        Proposition::integerRange(-10, 10) // Values from here will be passed as $b
-    )
-    // ...then we define the function to run with check().
-    ->check(function($a, $b) {
-        // Typically you would use an asser in here, not an if.
-
-        if ($a > $b) {
-            // It was correct: indicate success!
-            echo ("Yup, $a is bigger than $b\n");
+    ->given(Proposition::integers(), Proposition::integers())
+    ->call(function($a, $b) {
+        if(equal_magnitude($a, $b) != (abs($a) == abs($b))) {
+            echo "ERROR!! it is wrong when \$a is $a and \$b is $b!!\n";
         } else {
-            // It was not correct, indicate error!
-            echo ("NO! $a is NOT bigger than $b!!\n");
+            echo "it seems to work when \$a is $a and \$b is $b.\n";
         }
-
     });
+
+// Oops! We're getting a lot of error messages. We will quickly discover that we used = where we should have used == in
+// our function.
+
+// In practice, you will be using this to pass a huge, automatically generated range of values into your functions and
+// check that some property always holds. You might for example want to check that the output of a function has a
+// certain relationship to your input, that the function did or did not throw an exception, and so on.
